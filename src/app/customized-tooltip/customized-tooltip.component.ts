c4f8d109c591bccdadcb15e7b4fdb02077360327
ugrasenanv <ugrasenanv@gmail.com>
2020-02-22 23:08:23 +0530

    4 files modified, 18 deleted

--- src/app/customized-tooltip/customized-tooltip.component.ts
+++ src/app/customized-tooltip/customized-tooltip.component.ts
@@ -9,25 +9,12 @@ import { ConfigurationOptions, TooltipOptionsEnum } from 'intl-input-phone';
 export class CustomizedTooltipComponent implements OnInit {
 
   configOption1: ConfigurationOptions;
-  configOption2: ConfigurationOptions;
-  configOption3: ConfigurationOptions;
-  configOption4: ConfigurationOptions;
+
   constructor() {
     this.configOption1 = new ConfigurationOptions();
     this.configOption1.SelectorClass = "ToolTipType1";
     
 
-    this.configOption2 = new ConfigurationOptions();
-    this.configOption2.SelectorClass = "ToolTipType2";
-    this.configOption2.ToolTipText = TooltipOptionsEnum.CountryISOCode;
-
-    this.configOption3 = new ConfigurationOptions();
-    this.configOption3.SelectorClass = "ToolTipType3";
-    this.configOption3.ToolTipText = TooltipOptionsEnum.CountryPhoneCode;
-
-    this.configOption4 = new ConfigurationOptions();
-    this.configOption4.SelectorClass = "ToolTipType4";
-    this.configOption4.IsShowToolTip = false;
   }
 
   ngOnInit() {
